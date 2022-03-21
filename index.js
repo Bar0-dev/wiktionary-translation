f9b08253c2acbedcfb9bc8895896f2464bd40a42
Bartosz <bartosz@pop-os.localdomain>
2022-03-21 16:27:34 +0100

    refactoring

--- index.js
+++ index.js
@@ -6,54 +6,75 @@ const propIwLinksQuery = (title, trgtLang) =>
 const propLinksQuery = (title) =>
   `&action=query&prop=links&pllimit=max&titles=${title}&format=json`;
 
-const getData = async (title, srcLang, trgtLang) => {
-  let response = null;
-  let data = {};
-  let parsedTitle = "";
+const getData = async (endpoint, props) => {
   try {
-    response = await axios.get(
-      endpoint(srcLang) + propIwLinksQuery(title, trgtLang)
-    );
-    [data] = Object.values(response.data.query.pages);
-    if (data.iwlinks) {
-      return data.iwlinks;
-    } else {
-      response = await axios.get(
-        endpoint(srcLang) + propIwLinksQuery(title + "/translations", trgtLang)
-      );
+    let data = null;
+    const response = await axios.get(endpoint + props);
+    if (response.status === 200) {
       [data] = Object.values(response.data.query.pages);
-    }
-    if (data.iwlinks) {
-      return data.iwlinks;
+      return data;
     } else {
-      response = await axios.get(endpoint(srcLang) + propLinksQuery(title));
-      [data] = Object.values(response.data.query.pages);
-      parsedTitle = data.links.find((entry) => entry.ns === 0).title;
-      response = await axios.get(
-        endpoint(srcLang) + propIwLinksQuery(parsedTitle, trgtLang)
-      );
-      if()
+      throw response.error;
     }
-    [data] = Object.values(response.data.query.pages);
-
-    return data;
   } catch (error) {
     console.log(error);
   }
 };
 
 const getTranslations = async (title, srcLang, trgtLang) => {
-  let translations = [];
-  let data = await getData(title, srcLang, trgtLang);
-  if (data.length) {
-    const translations = data.map((entries) => entries["*"]);
-    console.log(translations);
-    return translations;
+  try {
+    const targetEndpoint = endpoint(srcLang);
+    const returnCallback = (entry) => entry["*"];
+    let response = await getData(
+      targetEndpoint,
+      propIwLinksQuery(title, trgtLang)
+    );
+    console.log(response);
+    if (!response) return false;
+    if (response.iwlinks) {
+      return response.iwlinks.map(returnCallback);
+    } else {
+      response = await getData(
+        targetEndpoint,
+        propIwLinksQuery(title + "/translations", trgtLang)
+      );
+    }
+    if (response.iwlinks) {
+      return response.iwlinks.map(returnCallback);
+    } else {
+      response = await getData(targetEndpoint, propLinksQuery(title));
+      parsedTitle = response.links.find((entry) => entry.ns === 0).title;
+      response = await getData(
+        targetEndpoint,
+        propIwLinksQuery(parsedTitle, trgtLang)
+      );
+    }
+    if (response.iwlinks) {
+      return response.iwlinks.map(returnCallback);
+    } else {
+      throw new Error("No translation was found");
+      return false;
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
 
-getTranslations("wielding", "en", "pl");
-getTranslations("wield", "en", "pl");
-getTranslations("book", "en", "pl");
-getTranslations("chair", "en", "pl");
-getTranslations("tycker", "sv", "pl");
+// getTranslations("wielding", "en", "pl");
+// getTranslations("wield", "en", "pl");
+// getTranslations("book", "en", "pl");
+// getTranslations("chair", "en", "pl");
+// getTranslations("tycker", "sv", "pl");
+
+const testFunc = async () => {
+  //   const data = await getTranslations("wielding", "en", "pl");
+  //   const data = await getTranslations("wield", "en", "de");
+  //   const data = await getTranslations("book", "en", "pl");
+  //   const data = await getTranslations("chair", "en", "sv");
+  //   const data = await getTranslations("tycker", "sv", "pl");
+  //   const data = await getTranslations("cáscara", "es", "en");
+  const data = await getTranslations(encodeURI("puszka"), "pl", "en");
+  console.log(data);
+};
+
+testFunc();
